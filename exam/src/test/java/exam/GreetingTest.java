df25f4a9fb4ca64d9d356ac0fabf1c74a0f5163c
Patrick Zhong <54873076+pzhong2098@users.noreply.github.com>
2020-03-31 13:12:29 -0500

    Updated test names. This should pass.

--- exam/src/test/java/exam/GreetingTest.java
+++ exam/src/test/java/exam/GreetingTest.java
@@ -7,23 +7,23 @@ import org.junit.Test;
 public class GreetingTest {
 
 	@Test
-	public void test() 
+	public void testHello1() 
 	{
 		assertEquals("Hello Bob", Exam.getGreeting("Bob"));
 	}
 	
 	@Test
-	public void test2() 
+	public void testHello2() 
 	{
 		assertEquals("Hello a", Exam.getGreeting("a"));
 	}
 	@Test
-	public void testGoodBye() 
+	public void testGoodBye1() 
 	{
 		assertEquals("Goodbye a", Exam.getGoodBye("a"));
 	}
 	@Test
-	public void testGoodBye() 
+	public void testGoodBye2() 
 	{
 		assertEquals("Goodbye Jim", Exam.getGoodBye("Jim"));
 	}
